d52a17b396098a43c5acbeb4d3ce2bfcb35cabd5
OscarLGH <OscarLGH@live.com>
2019-12-11 18:11:53 +0800

    Inital commit.

---
+++ setup_script/setup_debian.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+apt install \
+gcc uuid-dev g++ acpica-tools nasm \
+build-essential autogen autoconf automake bison gawk m4 flex qt4-dev-tools libncurses5-dev libssl-dev  \
+libglib2.0-dev libpixman-1-dev libsdl2-2.0 libsdl2-dev \
+vnc4server 
