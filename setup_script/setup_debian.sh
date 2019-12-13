1bcbbcb936d33edafeb728579f24a0618d37ac24
OscarLGH <OscarLGH@live.com>
2019-12-13 13:33:38 +0800

    Add vim setup script.

--- setup_script/setup_debian.sh
+++ setup_script/setup_debian.sh
@@ -4,4 +4,5 @@ apt install \
 gcc uuid-dev g++ acpica-tools nasm \
 build-essential autogen autoconf automake bison gawk m4 flex qt4-dev-tools libncurses5-dev libssl-dev  \
 libglib2.0-dev libpixman-1-dev libsdl2-2.0 libsdl2-dev \
-vnc4server 
+vnc4server \
+cmake cscope ctags libclang-dev python-dev 
